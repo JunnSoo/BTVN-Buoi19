565608b4b86e0119bc4016c229d4171b14547040
JunnSoo <105549610+JunnSoo@users.noreply.github.com>
2025-07-27 14:08:49 +0700

    Add files via upload

---
+++ btvn.java
@@ -0,0 +1,26 @@
+package btvnservlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name = "btvn", urlPatterns = {"/login"})
+public class btvn extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		System.out.println("Gia tri: " + username + " - " + password);
+		req.getRequestDispatcher("login.html").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		System.out.println("Gia tri 1: " + username + " - " + password);
+	}
+}
